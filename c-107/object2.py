b879e11e1d52c7fb6c77cad6375f4df4c75996e7
SarayuNidjelli <74332513+SarayuNidjelli@users.noreply.github.com>
2022-09-11 18:53:12 -0500

    Add files via upload

---
+++ c-107/object2.py
@@ -0,0 +1,33 @@
+import cv2
+import time
+import math
+
+video = cv2.VideoCapture("bb3.mp4")
+tracker=cv2.TrackerCSRT_create()
+returned, img = video.read()
+bbox = cv2.selectROI("tracking",img,False)
+tracker.init(img,bbox)
+print(bbox)
+def drawBox(img,bbox):
+    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255,0),3,1)
+    cv2.putText(img,"tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+    while True:
+        check,img = video.read()   
+        success,bbox = tracker.update(img)
+        if success:
+            drawBox(img,bbox)
+        else:
+            cv2.putText(img,"lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        cv2.imshow("result",img)
+            
+        key = cv2.waitKey(25)
+
+        if key == 32:
+            print("Stopped!")
+            break
+
+
+video.release()
+cv2.destroyALLwindows()
+
